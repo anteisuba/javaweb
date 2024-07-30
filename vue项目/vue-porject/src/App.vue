333a2b33519b0135ca52b75a94d902252d1136b6
fulina <xiuruisu@gmail.com>
2024-07-30 22:22:32 +0900

    2024 7 29

---
+++ vue项目/vue-porject/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- <h1>{{ message }}</h1> -->
+    <!-- <element-view></element-view> -->
+    <!-- 员工管理 -->
+    <emp-view></emp-view>
+
+  </div>
+</template>
+
+<script>
+// import ElementView from "./views/element/ElementView.vue";
+import EmpView from './views/tlias/EmpView.vue';
+export default {
+  components:{EmpView},
+  data() {
+    return {
+      message:"Hello Vue"
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+
+</style>
